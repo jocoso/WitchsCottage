3f7efd94c8577dc643c1c433a19dffe81fd2afbc
jocoso <joshua.collado022@gmail.com>
2022-05-01 17:39:22 -0400

    Front: 'Created welcome page'

--- witchscottage-front/src/Component/Pages/welcome.js
+++ witchscottage-front/src/Component/Pages/welcome.js
@@ -1,1 +1,33 @@
 import React from 'react';
+
+// Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Libraries
+import PropType from 'prop-types';
+
+class WelcomePage extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <h1>Welcome</h1>
+        );
+    }
+}
+
+// PropType
+WelcomePage.propTypes = {
+
+};
+
+export default WelcomePage;
